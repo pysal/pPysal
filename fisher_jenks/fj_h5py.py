0e93c8d23fb66a73c97267d86b7f66e8dc6e5585
Jay <jlaura@asu.edu>
2013-06-18 12:34:22 -0700

    HDF5 Working

--- fisher_jenks/fj_h5py.py
+++ fisher_jenks/fj_h5py.py
@@ -4,10 +4,29 @@ import multiprocessing
 import ctypes
 import warnings
 import h5py
-from sys import stdout
 
 #Suppress the divide by zero errors
 warnings.filterwarnings('ignore', category=RuntimeWarning)
+
+#We can not use nested functions with map.
+def calcVar(arrRow):
+    '''This function calculates the diameter matrix.  It is called by fj.
+    The return line performs the calculation.  All other lines prepare an
+    order vector, prepended with zeros when necesary which stores n, the number 
+    of elements summed for each index.'''
+
+    lenN = (arrRow != 0).sum()
+    n = numpy.arange(1, lenN+1)
+    
+    if lenN != numVal:
+	n.resize(arrRow.shape[0]) 	
+	n[arrRow.shape[0]-lenN:] =  n[:lenN-arrRow.shape[0]] 
+	n[0:arrRow.shape[0]-lenN] = 0 
+
+    arrRow =  ((numpy.cumsum(numpy.square(arrRow))) - \
+            ((numpy.cumsum(arrRow)*numpy.cumsum(arrRow)) / (n)))
+    arrRow[numpy.isnan(arrRow)] = 0
+    return arrRow
 
 def fisher_jenks(values, classes=5, cores=None, sort=True):
     '''Fisher-Jenks Optimal Partitioning of an ordered array into k classes
@@ -61,147 +80,96 @@ def fisher_jenks(values, classes=5, cores=None, sort=True):
 	    if x != 0:
 		varMat[x,...,0:x] = 0
 
-	errCtypes = multiprocessing.RawArray(ctypes.c_double, classes*numVal)
-	errorMat = numpy.frombuffer(errCtypes)
-	errorMat.shape = (classes, numVal)
+	errorMat = numpy.zeros(shape=(classes, numVal))
 	
 	pivotShape = (classes, numVal)
 	pivotMat = numpy.ndarray(pivotShape, dtype=numpy.float)
 	
 	#Initialize the arrays as globals.
-	initArr(varMat, errorMat)
+	#initArr(varMat, errorMat)
 	
-	print varMatFile.mode
-	
-	return pivotMat, numClass
-    
-    def initArr(varMat_, errorMat_):
-	'''Initialize the ctypes arrays as global variables for multiprocessing'''
-	global sharedVar
-	sharedVar = varMat_
-	
-	global sharedErr
-	sharedErr = errorMat_
+	return pivotMat, numClass, varMatFile, errorMat, varMat
     
     def initErrRow(errRow_):
 	'''Initialize the sharedErrRow as a global for multiprocessing'''
 	global sharedErrRow
 	sharedErrRow = errRow_
-    
-    def fj(sharedVar,i, values, start):
-	'''This function facilitates passing multiple rows to each process and
-	then performing multiple vector calculations along individual rows.'''
-	sharedVar[i] = numpy.apply_along_axis(calcVar, 1, sharedVar[i], len(values))
-	if start > 1000:
-	    print sharedVar[2499]
-    
-    def calcVar(arrRow, lenValues):
-	'''This function calculates the diameter matrix.  It is called by fj.
-	The return line performs the calculation.  All other lines prepare an
-	order vector, prepended with zeros when necesary which stores n, the number 
-	of elements summed for each index.'''
-	
-	lenN = (arrRow != 0).sum()
-	n = numpy.arange(1, lenN+1)
-	
-	if lenN != lenValues:
-	    n.resize(arrRow.shape[0]) 	
-	    n[arrRow.shape[0]-lenN:] =  n[:lenN-arrRow.shape[0]] 
-	    n[0:arrRow.shape[0]-lenN] = 0 
-    
-	arrRow =  ((numpy.cumsum(numpy.square(arrRow))) - \
-	        ((numpy.cumsum(arrRow)*numpy.cumsum(arrRow)) / (n)))
-	arrRow[numpy.isnan(arrRow)] = 0
-	return arrRow
-    
+
     def err(row,y,step, lenrow):
 	'''This function computes the error on a segment of each error row, from the error matrix.  
 	The function is provided with the row number, starting index, step size, and total row length.
 	Since start + step could be greater than row length the first three lines check for this condition
 	and set the stop variable to the appropriate value'''
-    
+
 	stop = (y+step)
 	if stop+1 > lenrow:
 	    stop = lenrow-1
 	while y <= stop:
-	    sharedErrRow[y] = numpy.amin(sharedErr[row-1][row-1:y+row] + sharedVar[:,y+row][row:y+row+1])
+	    sharedErrRow[y] = numpy.amin(sharedErr[row-1][row-1:y+row] + varMatSlice[:,y+row][row:y+row+1])
 	    y+=1   
-    
+	print sharedErrRow
+	
     if sort:
 	values.sort()
     
     if cores == None:
 	cores = multiprocessing.cpu_count()
-	
+    
+    #Hacky, I think I can use a class to make this variable global,
+    # without polluting the namespace.
+    global numVal 
     numVal = len(values)
 
     #Allocate all necessary memory
-    pivotMat, k = allocate(values, classes)
-    print "Allocation Complete using HDF5"
+    pivotMat, k, varMatFile, errorMat, varMat = allocate(values, classes)
     
     #Calculate the number of cores over which to multiprocess
     if cores > len(values):
 	cores = len(values)
-    step = numVal // cores
-
+    pool = multiprocessing.Pool(cores)
+    
     t0 = time.time()
     jobs = []
-    
-    #Calculate the variance matrix
-    for i in range(0,len(values),step):
-	p = multiprocessing.Process(target=fj,args=(sharedVar,slice(i, i+step),values, i))
-	jobs.append(p) 
-    for job in jobs:
-	job.start()
-    for job in jobs:
-	job.join()
-    
-    del jobs[:], p, job
+
+    for x in range(0,numVal,cores):
+	iterable = [row for row in varMat[x:x+cores]]
+	results = pool.map(calcVar, iterable)
+	index = x
+	for result in results:
+	    varMat[index] = result
+	    index += 1
+	#Manual cleanup
+	del results
+
     t1 = time.time()
-    print "Variance Matrix Complete"
-    
+
     #Calculate the error matrix
-    sharedErr[0] = sharedVar[0]
-    
+    errorMat[0] = varMat[0]
+    print varMat
+    print "Computing Error Matrix"
     #Error Matrix Calculation
-    row = 1    
-    for x in sharedErr[1:]:
-	stdout.write("\rFinished Row {} / {}.".format(row, len(sharedErr[:,1]-1)))
-	errRow = x[row:]
-	#Initialize the errorRow as a global for multiprocessing writes
-	initErrRow(errRow)
-	
-	#Calculate the step size for the errRow to be processed
-	step = len(errRow) // cores
-	
-	for y in range(0,len(errRow), step+1):
-	    p = multiprocessing.Process(target=err, args=(row,y, step, len(errRow)))
-	    jobs.append(p)
-	
-	for job in jobs:
-	    job.start()
-	for job in jobs:
-	    job.join()
-	del jobs[:], p, job
-	 
-	row += 1
-	
+    row = 1
+    for x in errorMat[1:]: #Get each row, save the first
+	errRow = x[row:] #Work only on the indices which will hold valid values
+	for y in range(0,len(errRow)): #Iterate through each index in errRow
+	    errRow[y] = numpy.amin(errorMat[row-1][row-1:y+row] + varMat[:,y+row][row:y+row+1])
+	row += 1 #Iterate the row counter to get valid slices
+    
     t2 = time.time()
     
     #Calculate Pivots
     pivots = []
     j = k - 1
     col = numVal - 1
-    print numVal
     
     while j > 0:
-	ev = sharedErr[j, col]
+	ev = errorMat[j, col]
 	pivot_search = True
 	right = col
 	
 	while pivot_search:
-	    left_error = sharedErr[j-1, right-1]
-	    right_error = sharedVar[right, col]
+	    left_error = errorMat[j-1, right-1]
+	    right_error = varMat[right, col]
 	    if left_error + right_error == ev:
 		pivots.insert(0, right)
 		col = right -1
@@ -211,6 +179,7 @@ def fisher_jenks(values, classes=5, cores=None, sort=True):
     t3 = time.time()
 	
     print "Time: ", t3-t1
+    
     #Close the file internally, so that it definitely gets closed.
     varMatFile.close()
     
@@ -223,6 +192,11 @@ def _test():
 #Not called by time_test.py
 if __name__ == '__main__':
     #_test()
+    #x = [12,10.8, 11, 10.8, 10.8, 10.8, 10.6, 10.8, 10.3, 10.3, 10.3,10.4, 10.5, 10.2, 10.0, 9.9]
+    #p1 = fisher_jenks(x, 5)
+    #print p1  
+
+    
     x = []
     for y in range(500):
 	x.append(15)
@@ -230,6 +204,5 @@ if __name__ == '__main__':
 	x.append(2)
 	x.append(26)
 	x.append(191)
-    p = fisher_jenks(x, 5)
-    print p  
-    
+    p = fisher_jenks(x, 5)    
+    print p

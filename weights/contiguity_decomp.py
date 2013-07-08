4ec07884f3a96fe545e57a8b28e5aa6543a563d4
Serge Rey <sjsrey@gmail.com>
2013-07-08 15:45:06 -0700

    - working on decomposition

---
+++ weights/contiguity_decomp.py
@@ -0,0 +1,96 @@
+from IPython.parallel import Client
+from itertools import combinations
+
+client = Client()
+
+print client.ids
+
+ncpus = len(client.ids)
+
+import pysal as ps
+import numpy as np
+
+#sf = ps.open(ps.examples.get_path("nat.shp"))
+#sf = ps.open(ps.examples.get_path("columbus.shp"))
+sf = ps.open(ps.examples.get_path("sids2.shp"))
+bb = sf.bbox
+
+
+w = (bb[0] - bb[2]) / (ncpus-1)
+
+w = np.abs(w)
+
+bounds = np.arange(bb[0], bb[2]+w, w)[1:]
+bounds[-1] += 0.0001*w
+bins = {}
+for b in range(len(bounds)):
+    bins[b] = []
+
+shps = []
+
+for shp in sf:
+    shps.append(shp)
+
+    bbi = shp.bounding_box
+    left = np.nonzero((bounds > bbi.left))[0][0]
+    right = np.nonzero((bounds > bbi.right))[0][0]
+    bids = range(left, right+1)
+    for bid in bids:
+        bins[bid].append(shp)
+
+
+#sf.close()
+
+def bf_queen(shps):
+    n = len(shps)
+    w = {}
+    coords = {}
+
+    for i in range(n-1):
+        si = shps[i]
+        vertsi = si.vertices
+        for vi in vertsi:
+            if vi not in coords:
+                coords[vi] = set([i])
+            else:
+                coords[vi] = coords[vi].union(set([i]))
+        for j in range(i+1,n):
+            sj = shps[j]
+            vertsj = sj.vertices
+            for vj in vertsj:
+                if vj not in coords:
+                    coords[vj] = set([j])
+                else:
+                    coords[vj] = coords[vj].union(set([j]))
+    for coord in coords:
+        if len(coords[coord]) > 1:
+            pairs = combinations(coords[coord],2)
+            for pair in pairs:
+                l,r = pair
+                if l not in w:
+                    w[l] = set([r])
+                else:
+                    w[l] = w[l].union(set([r]))
+                if r not in w:
+                    w[r] = set([l])
+                else:
+                    w[r] = w[r].union(set([l]))
+    return w, coords
+
+import time
+t1 = time.time()
+res, c = bf_queen(shps)
+t2 = time.time()
+
+print 'Sequential: ',t2-t1
+
+# parallel
+t1 = time.time()
+view = client[0:-1]
+with client[:].sync_imports():
+    from itertools import combinations
+results = view.map(bf_queen, bins.values())
+t2 = time.time()
+print 'Parallel: ', t2-t1
+
+

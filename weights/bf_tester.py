9c3ffa10f4a687da63fbec703b8a4a3116f74083
Serge Rey <sjsrey@gmail.com>
2013-07-09 10:37:07 -0700

    - fixed logic bug in rook checker. now much faster and matches opengeoda with   regard to connectivity histogram

--- weights/bf_tester.py
+++ weights/bf_tester.py
@@ -54,68 +54,30 @@ def bf_contiguity(shps, wttype = "QUEEN"):
                 neighbors[r] = neighbors[r].union([l])
         return neighbors
     elif wttype.upper() == 'ROOK':
-        vertices = {}
-        for i, shp in enumerate(shps):
-            si = set([i])
-            for vertex in shp.vertices:
-                if vertex not in vertices:
-                    vertices[vertex] = set()
-                vertices[vertex] = vertices[vertex].union(si)
-        checked = []
         edges = {}
-        for vertex in vertices:
-            pairs = combinations(vertices[vertex], 2)
-            for l,r in pairs:
-                if l not in neighbors:
-                    neighbors[l] = set()
-                if r not in neighbors:
-                    neighbors[r] = set()
-                lr = set([l,r])
-                if checked.count(lr) == 0:
-                    checked.append(lr)
-                    # check for shared edge
-                    vl = shps[l].vertices
-                    vr = shps[r].vertices
-                    vls = set(vl)
-                    vrs = set(vr)
-                    common = vls.intersection(vrs)
-                    if len(common) >= 2:
-                        # at least two vertices in common
-                        # now check for sequential positioning
-                        good_left = []
-                        for el in combinations(common, 2):
-                            # check left poly for an edge defined 
-                            o,d = el
-                            # how many times does point appear
-                            co = vl.count(o) 
-                            cd = vl.count(d)
-                            sc = 0
-                            for po in range(co):
-                                lo = vl.index(o, sc)
-                                sd = 0
-                                for pd in range(cd):
-                                    ld = vl.index(d, sd)
-                                    diff = np.abs(lo - ld)
-                                    sd = ld + 1
-                                    if diff == 1:
-                                        good_left.append(el)
-                                sc = lo + 1
-                        for el in good_left:
-                            o,d = el
-                            co = vr.count(o)
-                            cd = vr.count(d)
-                            sc = 0
-                            for po in range(co):
-                                ro = vr.index(o, sc)
-                                sd = 0
-                                for pd in range(cd):
-                                    rd = vr.index(d, sd)
-                                    diff = np.abs(ro - rd)
-                                    sd = rd + 1
-                                    if diff == 1:
-                                        neighbors[l] = neighbors[l].union(set([r]))
-                                        neighbors[r] = neighbors[r].union(set([l]))
-                                sc = ro + 1
+        neighbors = {}
+        for i, shp in enumerate(shps):
+            neighbors[i] = set()
+            nv = len(shp.vertices)
+            for o in range(nv-1):
+                d = o + 1
+                edge = [shp.vertices[o], shp.vertices[d]]
+                edge.sort()
+                edge = tuple(edge)
+                if edge not in edges:
+                    edges[edge] = set()
+                edges[edge] = edges[edge].union([i])
+        checked = {}
+        for edge in edges:
+            pairs = combinations(edges[edge], 2)
+            for pair in pairs:
+                l,r = pair
+                if pair not in checked and (r,l) not in checked:
+                    neighbors[l] = neighbors[l].union([r])
+                    neighbors[r] = neighbors[r].union([l])
+                    checked[pair] = pair
+                    checked[(r,l)] = (r,l)
+
         return neighbors
     else:
         print "Weight type not supported: ", wttype
@@ -157,4 +119,3 @@ if __name__ == '__main__':
     rook = rf_shapefile(sf)
     t2 = time.time()
     print "National rook: ", t2-t1
-

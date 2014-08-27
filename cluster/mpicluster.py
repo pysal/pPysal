9b985ea1b1856b3bfa71cbfe1cdf090799b47a91
Jay <jlaura@asu.edu>
2014-08-26 20:26:58 -0700

    added mpi group call

--- cluster/mpicluster.py
+++ cluster/mpicluster.py
@@ -1,11 +1,15 @@
 from mpi4py import MPI
 
+#MPI Boilerplate
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 ncores = comm.Get_size()
 status = MPI.Status()
-name = MPI.Get_processor_name()
+host = MPI.Get_processor_name()
+
+group = comm.Get_group()
+
 
 for r in range(ncores):
     if r == rank:
-        print "I am core {} of {} on {}".format(r, ncores, name)
+        print "I am core {} of {} on {} in group {}".format(r, ncores, host, group)
